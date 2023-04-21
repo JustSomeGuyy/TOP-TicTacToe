8a52f23077b7aae218ddbc9a3589720a4c8bf762
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2023-04-21 18:54:08 -0500

    created functions for the form to open and close, added the argument for completing a row.

--- main.js
+++ main.js
@@ -1,36 +1,94 @@
 const gameBoard = document.querySelectorAll('#gameBoard div');
-let playerOneName = document.getElementById('playerOne');
+const form = document.querySelector('.form-popup');
 
-// creating the playable game spaces into an array format
-let playableSpaces = [];
+// Creates A New Player
+const players = [];
 
-for (let i = 0; i < gameBoard.length; i++){
-    playableSpaces.push(gameBoard[i]);
-}
-
-console.log(playableSpaces);
-
-// creates new players
-let players = [];
-
-function player(name, char){
+function player(name, symbol){
     this.name = name;
-    this.piece = char;
-    return this;
+    this.symbol = symbol;
 };
 
 function newPlayer(){
-    let name = document.getElementById('playerName').value;
-    let char = document.getElementById('gameChar').value;
-    let newPlayer = new player(name, char);
-    players.push(newPlayer);
+    if(players.length < 2){
+        let name = document.getElementById('playerName').value;
+        let symbol = players.length === 0 ? 'X' : 'O';
+        let newPlayer = new player(name, symbol);
+        players.push(newPlayer);
+        displayPlayers();
+        document.getElementById('popup').reset();
+        closeForm();
+    } else {
+        alert('No more players');
+    }
 };
 
-function display(){
-    
+function displayPlayers(){
+    let display = document.getElementById('playerNames');
+    display.innerHTML = '';
+
+    for(let j = 0; j < players.length; j++) {
+        let player = players[j];
+        let info = '<div class="names">' + '<p>Player: ' + player.name + '</p>' + '<p>Symbol: ' + player.symbol + '</p></div>';
+        display.innerHTML += info;
+    }
 }
 
-// const playerOne = player('Tyler', 'X');
-// const playerTwo = player('AI', 'O');
+// creates the pop-up for the player form
+function openForm() {
+  document.getElementById("popup").style.display = "block";
+}
 
-console.log(players);
+function closeForm() {
+  document.getElementById("popup").style.display = "none";
+}
+
+// Creates a computer brain
+
+
+// Functions for playing the game
+let currentPlayer = players[0];
+let gameEnded = false;
+
+for (let i = 1; i <= 9; i++) {
+    document.getElementById(i.toString()).addEventListener(
+    "click", 
+    function() {
+        if (this.innerHTML === "" && !gameEnded) {
+                this.innerHTML = currentPlayer.symbol;
+                this.classList.add(currentPlayer.symbol.toLowerCase());
+                checkWin();
+            if (currentPlayer === players[0]){
+                currentPlayer = players[1];
+            }else{
+                currentPlayer = players[0];
+            }
+            } else {
+                alert("Can't play here!")
+            }
+        }
+    );
+}
+// Checks to see if a row was made or not
+let winPos = [
+    [1, 2, 3], [4, 5, 6], 
+    [7, 8, 9], [1, 4, 7], 
+    [2, 5, 8], [3, 6, 9], 
+    [1, 5, 9], [3, 5, 7]
+];
+
+function checkWin() {
+    for (let i = 0; i < winPos.length; i++) {
+        if (
+            document.getElementById(winPos[i][0]).innerHTML === currentPlayer.symbol &&
+            document.getElementById(winPos[i][1]).innerHTML === currentPlayer.symbol &&
+            document.getElementById(winPos[i][2]).innerHTML === currentPlayer.symbol
+            ) {
+                document.getElementById(winPos[i][0]).classList.add("win");document.getElementById(winPos[i][1]).classList.add("win");document.getElementById(winPos[i][2]).classList.add("win"); gameEnded = true;
+                
+                setTimeout(function() {
+                    alert(currentPlayer.name + " wins!");
+                }, 500);
+            }
+    }
+}
